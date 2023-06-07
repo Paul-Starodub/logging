bc3cee429dfc6bfd53da932da65dbf9c7d08e23f
Paul Starodub <amorallex@gmail.com>
2023-06-07 08:38:23 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ print(logger)
 def main(name):
     logger.warning(f"Enter in the main function: name = {name}")
 
+# layer of notifications
+# notset - 0
+# debug - 10
+# info - 20
+# warning - 30
+# error - 40
+# critical - 50
+
 
 if __name__ == "__main__":
     main("paul")
